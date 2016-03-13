8ad67cdbcc80dea1914812f330e60624a212c1ee
Juuso Ansaharju <juuso.ansaharju@gmail.com>
2016-03-14 01:42:36 +0200

    Create CyclicRotation.js

---
+++ CyclicRotation.js
@@ -0,0 +1,11 @@
+function solution(A, K) {
+  'use strict';
+    
+  // return non-rotateable array right away
+  if(A.length < 2) return A;
+    
+  // rotate
+  for(let i = 0; i < K; i++) A.unshift(A.pop());
+    
+  return A;
+}
