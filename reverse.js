2d3c1be2582e061c74e6d3b2d7804b38e5b13151
Juuso Ansaharju <juuso.ansaharju@gmail.com>
2016-03-14 23:47:39 +0200

    Create reverse.js

---
+++ reverse.js
@@ -0,0 +1,16 @@
+function reverse(arr) {
+  'use strict';
+
+  let i = 0, 
+      len = arr.length,
+      j = len - 1,
+      half = len / 2;
+      
+  while(i < half) {
+    let temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+    i++; j--;
+  }
+  return arr;
+}
